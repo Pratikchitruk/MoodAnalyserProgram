f28bc6055d7316a91afecb2b694b766c8b84be3a
PratikChitruk <pratik.chitruk@gmail.com>
2022-06-14 12:25:53 +0530

    [pratik]Refactor : Taking mood message in constructor and test cases - implemented.

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" \n Implementation of Mood Analyser program..");
+        }
+    }
+}
